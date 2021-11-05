aeb4ad968fd98f607f1822b564c4922aa6e44814
samuelkll <samuel.bandeira.oliveira@gmail.com>
2021-11-04 23:26:59 -0300

    upgrade to the aplication

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+if(process.env.NODE_ENV !== 'production') 
+    require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const expressLayouts = require('express-ejs-layouts');
+const indexRoute = require('./routes/index');
+const mongoose = require('mongoose');
 
-app.set('engine view', 'ejs');
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+db.on('error', error => console.log(error));
+db.once('open', () => {
+    console.log('connected to mongo');
+})
+app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+
+app.use('/', indexRoute);
+
 app.listen(PORT, () => {
     console.log(`connected on port ${PORT}`);
 });
