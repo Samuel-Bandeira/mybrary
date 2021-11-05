e689f6c2905694cc37459425c8c7a7c2493d726d
samuelkll <samuel.bandeira.oliveira@gmail.com>
2021-11-04 22:29:17 -0300

    third commit

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
+const expressLayouts = require('express-ejs-layouts');
 
+app.set('engine view', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
 app.listen(PORT, () => {
     console.log(`connected on port ${PORT}`);
 });
