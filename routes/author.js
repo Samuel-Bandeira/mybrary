cf17b77f9c9bd8b81d178caecaa158985f2aa57e
samuelkll <samuel.bandeira.oliveira@gmail.com>
2021-11-06 15:07:20 -0300

    post commit

--- routes/author.js
+++ routes/author.js
@@ -1,18 +1,36 @@
 const express = require('express');
+const author = require('../models/author');
 const router = express.Router();
 const Author = require('../models/author');
 
 router.get('/', (req, res) => {
-    res.render('./author/index');
+    res.render('./authors/index');
 })
 
 router.get('/new', (req, res) => {
-    res.render('./author/new', {author: new Author()});
+    res.render('./authors/new', {author: new Author()});
 })
 
-router.post('/', (req, res) => {
-    const {name} = req.body;
-    console.log(name);
-})
+router.post('/', async (req, res) => {
+    const newAuthor = new Author({
+        name: req.body.name,
+    });
+    try {
+        const newAuthor = await author.save();
+    } catch(error) {
+
+    }
+    newAuthor.save((err, newAut) => {
+        if(err) {
+            res.render('./authors/new', {
+                author: newAuthor, 
+                errorMessage: 'Error creating author',
+            });
+        } else {
+            //res.redirect(`./authors/${newAut._id}`);
+            res.redirect('./authors');
+        }
+    })
+});
 
 module.exports = router;
