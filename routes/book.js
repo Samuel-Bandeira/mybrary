f683e92cdb77571ab3ceec81ab88b2ef45fcc7c9
samuelkll <samuel.bandeira.oliveira@gmail.com>
2021-11-13 14:06:37 -0300

    finishing book routes

--- routes/book.js
+++ routes/book.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const Book = require('../models/book');
 const Author = require('../models/author');
+const path = require('path');
+const uploadPath = path.Join('public', Book.coverImageBasePath);
+const imageMimeTypes = ['images/jpeg', 'images/png', 'images/gif'];
+const multer = require('multer');
+const upload = multer({
+    dest: uploadPath,
+    fileFilter: (req, file, callback) => {
+        callback(null, imageMimeTypes.includes(file.mimetype));
+    }
+});
 
 router.get('/', async (req, res) => {
     
@@ -20,8 +30,23 @@ router.get('/new', async (req, res) => {
     }
 });
 
-router.post('/', (req, res) => {
+router.post('/', upload.single('cover'), async (req, res) => {
+    const fileName = req.file != null ? req.file.filename : null;
 
+    const book = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        publishDate: new Date(req.body.publishDate),
+        pageCount: req.body.pageCount,
+        description: req.body.description,
+        coverImageName: fileName, 
+    });
+
+    try {
+        res.redirect('/books');
+    } catch {
+        res.redirect('/books/new')
+    }
 });
 
 module.exports = router;
