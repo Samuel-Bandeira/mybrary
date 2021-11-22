d219578213abf93aee474817b9a119ec2c4f4ca0
samuelkll <samuel.bandeira.oliveira@gmail.com>
2021-11-22 15:28:24 -0300

    blocking public images

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const Book = require('../models/book');
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get('/', async(req, res) => { 
+    let query = Book.find()
+    query = query.sort({ createdAt: 'desc'}).limit(2);
+    try {
+        books = await query.exec();
+        res.render('./index', {
+            books: books,
+        });
+    } catch {
+        res.redirect('/');
+    }
+    
 });
 
 module.exports = router;
